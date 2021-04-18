8a439ece8d2b96404ea7252cadb83fd311d03e83
ynportfolios <y.n.portfolios@gmail.com>
2021-04-18 16:25:25 +0900

    Implement issue crud

---
+++ api/app/controllers/issues_controller.rb
@@ -0,0 +1,27 @@
+class IssuesController < ApplicationController
+
+  def index
+    @issue = Issue.all
+    render json: @issue
+  end
+
+  def create
+    @issue = Issue.create(name: params[:name])
+    render json: @issue
+  end
+
+  def update
+    @issue = Issue.find(params[:id])
+    @issue.update_attributes(name: params[:name])
+    render json: @issue
+  end
+
+  def destroy
+    @issue = Issue.find(params[:id])
+    if @issue.destroy
+      head :no_content, status: :ok
+    else
+      render json: @issue.errors, status: :unproccessable_entity
+    end
+  end
+end
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_04_18_061006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "issues", force: :cascade do |t|
+    t.text "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
